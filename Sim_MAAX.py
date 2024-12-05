Added Earth with rotation, fixed up some other code

Sim_MAAX.py
@@ -8,6 +8,7 @@ from argparse import ArgumentParser, RawDescriptionHelpFormatter
 import vis_utils as vutil
 from vis_utils import coords_to_xyz, set_shaders, create_canvas, create_aurora_coords, update_aurora, create_aurora, add_earth
 from vispy.geometry import create_sphere
+from vispy.visuals.transforms import STTransform, MatrixTransform
 
 parser = ArgumentParser(description=__doc__,
                         formatter_class=RawDescriptionHelpFormatter)
@@ -39,8 +40,13 @@ parser.add_argument("-ca", "--camera_altitude", default=7, type=float,
 parser.add_argument("-start", "--starting_index", default=0, type=int,
                     help="Sets the starting index of iono files - Mainly useful when " +
                          "restarting the run. Default is 0.")
-parser.add_argument("-cb", "--colorbar", default=False, type=bool,
-                    help="Adds a color bar. Default is false.")
+parser.add_argument("-end", "--ending_index", default=-1, type=int,
+                    help="Sets the ending index of iono files - Mainly useful when " +
+                         "restarting the run. Default is -1, which will find the number of"
+                         "iono files.")
+parser.add_argument("-texture_path", "--texture_path", default='Earth.png', type=str,
+                    help="Path to earth texture used to render the Earth. Default is 'Earth.png'. "
+                         "Set to None to not load earth texture. ")
 args = parser.parse_args()
 
 path = args.path
@@ -50,13 +56,11 @@ ie_path = path + 'IE/'
 outdir = args.outdir
 
 iono_files = [f for f in sorted(os.listdir(ie_path)) if f.endswith('.idl.gz')]
-iono_start = rim.Iono(ie_path + iono_files[0])
+iono_start = rim.Iono(ie_path + iono_files[args.starting_index])
+iono_start['n_sigmah'][iono_start['n_sigmah'] < 0.55] = 10
+iono_start['s_sigmah'][iono_start['s_sigmah'] < 0.55] = 10
 
 aurora_canvas, sat_views = create_canvas(args.cameras, camera_altitude=args.camera_altitude)
-
-for j, view in enumerate(sat_views):
-    view.camera.transform.rotate(args.camera_separation * j, [1, 0, 0])
-    aurora_canvas.render()
 
 if not os.path.exists(outdir):
     os.makedirs(outdir)
@@ -69,19 +73,39 @@ if args.camera_rotation is None:
     rotation = v / (r_earth * args.camera_altitude) * 180 / np.pi * 60
 else:
     rotation = args.camera_rotation
-
-for view in sat_views:
+earth_rotation = 0.25
+earths = []
+light_dirs = []
+for j, view in enumerate(sat_views):
+    view.camera.depth_value = 1
+    earths.append(add_earth(view, texture_path=args.texture_path))
+    aurora_canvas.render()
+    view.camera.transform.rotate(args.camera_separation * j, [1, 0, 0])
+    aurora_canvas.render()
     view.camera.transform.rotate(rotation * args.starting_index, [1, 0, 0])
-    # add_earth(view)
+    aurora_canvas.render()
+    start_time = iono_start.attrs['time']
+    earths[-1].transform = MatrixTransform()
+    light_dirs.append(earths[-1].transform.map((-10, 0, 0)))
+    earths[-1].transform.rotate(earth_rotation * (start_time.hour * 60 + start_time.minute), [0, 0, 1])
+    earths[-1].mesh.shading_filter.light_dir = earths[-1].transform.imap(light_dirs[-1])[:3]
+    aurora_canvas.render()
+
 
 n_auroras, s_auroras = create_aurora(iono_start, sat_views, minz=args.minz, maxz=args.maxz)
 
-for i in range(args.starting_index, len(iono_files)):
-    for view in sat_views:
+if args.ending_index == -1:
+    end_point = len(iono_files)
+else:
+    end_point = args.ending_index
+for i in range(args.starting_index, end_point):
+    for view, earth, light_dir in zip(sat_views, earths, light_dirs):
         view.camera.transform.rotate(rotation, [1, 0, 0])
+        earth.transform.rotate(earth_rotation, [0, 0, 1])
+        earth.mesh.shading_filter.light_dir = earth.transform.imap(light_dir)[:3]
     iono = rim.Iono(ie_path + iono_files[i])
     update_aurora(iono, n_auroras, s_auroras, maxz=args.maxz, minz=args.minz)
     aurora_canvas.update()
     img = aurora_canvas.render()
 
-    io.image.imsave(f'{outdir}/frame{i:04d}.png', img)
+    io.image.imsave(f'{outdir}/frame{i:04d}{iono.attrs["time"]:%Y%m%d_%H%M%S}.png', img)

vis_utils.py
@@ -1,10 +1,12 @@
 import numpy as np
 from vispy import app, scene
+from vispy.io import imread
 import os
 from spacepy.pybats import bats, rim
 from matplotlib import colormaps
 from vispy.geometry.generation import create_sphere
 from spacepy.pybats.batsmath import d_dx, d_dy
+from vispy.visuals.filters import TextureFilter
 
 
 #%% Point Stuffs
@@ -67,7 +69,7 @@ def find_outer_points(x, y, z, num_bins=45):
 #%% Vispy Stuffs
 
 
-def set_shaders(mesh, diffuse=0, specular=0, ambient=1):
+def set_shaders(mesh, diffuse=0.0, specular=0.0, ambient=1.0, light_dir=(-10, 0, 0)):
     """
     Sets the shading of a mesh object based on parameters.
 
@@ -88,12 +90,23 @@ def set_shaders(mesh, diffuse=0, specular=0, ambient=1):
     ""
     """
     mesh.shading_filter.diffuse_light = (1, 1, 1, diffuse)
+    mesh.shading_filter.light_dir = light_dir
     mesh.shading_filter.ambient_light = (1, 1, 1, ambient)
     mesh.shading_filter.specular_light = (1, 1, 1, specular)
 
-def add_earth(view):
-    scene.visuals.Sphere(radius=1, method='latitude', parent=view.scene,
-                                   color=[0, 0, 1, 1])
+def add_earth(view, texture_path=None):
+    earth = scene.visuals.Sphere(radius=1, method='latitude', parent=view.scene,
+                                   color=[1, 1, 1, 1], shading='smooth', cols=2048, rows=1024)
+    if texture_path is not None:
+        texture = imread(texture_path)
+        longitude = np.linspace(0, 1, texture.shape[0])
+        latitude = np.linspace(0, 1, texture.shape[1])
+        texcoords = np.dstack(np.meshgrid(latitude, longitude)).reshape(-1, 2)
+        texcoords = np.delete(texcoords, np.arange(4096, texcoords.shape[0], 4096), axis=0)
+        texture_filter = TextureFilter(texture, texcoords=texcoords)
+        earth.mesh.attach(texture_filter)
+    set_shaders(earth.mesh, diffuse=1, ambient=0.2)
+    return earth
 
 def create_canvas(num_cams=1, camera_altitude=7, camera_type='arcball', resolution=(3840, 2160), interactive=False,
                   center=(0, 0, 0), fov=20):
@@ -151,7 +164,7 @@ def create_aurora_coords(iono, views):
     return n_aurora, s_aurora
 
 
-def update_aurora(iono, n_aurora, s_aurora, colormap1='hot', colormap2='viridis', minz=2, maxz=20):
+def update_aurora(iono, n_aurora, s_aurora, colormap1='hot', colormap2='hot', minz=2, maxz=20, transparency_min=0.0):
     """
     Update the aurora surface plots with new iono conductivity data
 
@@ -173,13 +186,15 @@ def update_aurora(iono, n_aurora, s_aurora, colormap1='hot', colormap2='viridis'
     n_hall_cond = iono['n_sigmah']
     n_norm_bright = (n_hall_cond - minz) / (maxz - minz)
     n_aurora_colors = aurora_cmap(n_norm_bright)
-    # n_aurora_colors[:, :, 3][n_norm_bright < transparency_min] = 0
+    n_aurora_colors[:, :, 3] = n_norm_bright + 0.2
+    n_aurora_colors[:, :, 3][n_norm_bright < transparency_min] = 0
 
     aurora_cmap = colormaps[colormap2]
     s_hall_cond = iono['s_sigmah']
     s_norm_bright = (s_hall_cond - minz) / (maxz - minz)
     s_aurora_colors = aurora_cmap(s_norm_bright)
-    # s_aurora_colors[:, :, 3][s_norm_bright < transparency_min] = 0
+    s_aurora_colors[:, :, 3] = s_norm_bright + 0.2
+    s_aurora_colors[:, :, 3][s_norm_bright < transparency_min] = 0
 
     for i in range(len(n_aurora)):
         n_aurora[i].set_data(colors=n_aurora_colors)